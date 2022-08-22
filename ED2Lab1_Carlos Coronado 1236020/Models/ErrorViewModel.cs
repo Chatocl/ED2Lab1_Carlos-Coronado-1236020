409a4f17d92017232fe5176751e54e3d8d4107d9
CARLOS ROBERTO CORONADO LAZO <crcoronado@correo.url.edu.gt>
2022-08-22 07:21:53 -0600

    Agregar archivos de proyecto.

---
+++ ED2Lab1_Carlos Coronado 1236020/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ED2Lab1_Carlos_Coronado_1236020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
